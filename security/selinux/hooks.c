b145eb3f5680998c25ca8f59704edf2d19fb5c7f
minhker <minhkk0248@gmail.com>
2020-08-03 22:29:06 +0700

    add ccache,remove in kernel name,selinux to perrmision,drivers/mmc: spi CRC configurable,char: random: Fix Deep Sleep issue

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -210,7 +210,7 @@ static int __init enforcing_setup(char *str)
 #ifdef CONFIG_ALWAYS_ENFORCE
 		selinux_enforcing = 1;
 #else
-		selinux_enforcing = enforcing ? 1 : 0;
+		selinux_enforcing = enforcing ? 0 : 0;
 #endif
 // ] SEC_SELINUX_PORTING_COMMON
 	return 1;
